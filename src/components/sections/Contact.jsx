ded6e0814a0ea042374e314fb59c17ff40fc50f4
Kevin Andersson <kevin200032@live.se>
2025-03-12 15:43:39 +0100

    Fix spacing

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -39,19 +39,19 @@ function Contact({menuOpen}) {
       </h2>
       <form className="space-y-5" onSubmit={handleSubmit}>
         <div className="md:relative flex justify-center items-center">
-            <input type="text" id="name" name="name" required value={formData.name} className=" w-3/4 md:w-full bg-white/5 border border-white/10 rounded-lg px-4 py-3 text-white transition focus:outline-none focus:border-purple-500 focus:bg-purple-500/10" placeholder="Name..." onChange={(e) => setFormData({...formData, name: e.target.value })} />
+            <input type="text" id="name" name="name" required value={formData.name} className=" w-3/5 md:w-full bg-white/5 border border-white/10 rounded-lg px-4 py-3 text-white transition focus:outline-none focus:border-purple-500 focus:bg-purple-500/10" placeholder="Name..." onChange={(e) => setFormData({...formData, name: e.target.value })} />
         </div>
 
         <div className="md:relative flex justify-center items-center">
-            <input type="email" id="email" name="email" required value={formData.email} className="w-3/4 md:w-full bg-white/5 border border-white/10 rounded-lg px-4 py-3 text-white transition focus:outline-none focus:border-purple-500 focus:bg-purple-500/10" placeholder="example@gmail.com"
+            <input type="email" id="email" name="email" required value={formData.email} className="w-3/5 md:w-full bg-white/5 border border-white/10 rounded-lg px-4 py-3 text-white transition focus:outline-none focus:border-purple-500 focus:bg-purple-500/10" placeholder="example@gmail.com"
             onChange={(e) => setFormData({...formData, email: e.target.value })} />
         </div>
 
         <div className="md:relative flex justify-center items-center">
-            <textarea id="message" name="message" required value={formData.message} rows={5} className=" w-3/4 md:w-full bg-white/5 border border-white/10 rounded-lg px-4 py-3 text-white transition focus:outline-none focus:border-purple-500 focus:bg-purple-500/10" placeholder="Your Message..." onChange={(e) => setFormData({...formData, message: e.target.value })} />
+            <textarea id="message" name="message" required value={formData.message} rows={5} className=" w-3/5 md:w-full bg-white/5 border border-white/10 rounded-lg px-4 py-3 text-white transition focus:outline-none focus:border-purple-500 focus:bg-purple-500/10" placeholder="Your Message..." onChange={(e) => setFormData({...formData, message: e.target.value })} />
         </div>
         <div className="flex justify-center">
-        <button type="submit" className="w-3/4 md:w-full  bg-purple-600 text-white py-3 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)] cursor-pointer">
+        <button type="submit" className="w-3/5 md:w-full  bg-purple-600 text-white py-3 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)] cursor-pointer">
           Send Message
         </button>
         </div>
--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -3,7 +3,7 @@ import RevealOnScroll from "../RevealOnScroll";
 function Home({menuOpen}) {
 
   return(
-    <section id="home" className={`min-h-screen flex items-center justify-center relative transition-all duration-500 ${menuOpen ? "opacity-10" : "opacity-100"}`}>
+    <section id="home" className={`min-h-screen flex items-center justify-center relative transition-all duration-500 ${menuOpen ? "opacity-10" : "opacity-100"} px-4 md:px-0`}>
     <RevealOnScroll>
       <div className={`text-center z-10 px-4`}>
         <h1 className="text-4xl md:text-6xl font-bold mb-6
