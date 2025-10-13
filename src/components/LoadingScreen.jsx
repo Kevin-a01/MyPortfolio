c290b1c06a8233b29c0d5c52451ef87f793869c5
Kevin Andersson <kevin200032@live.se>
2025-10-13 16:19:55 +0200

    fix loading message

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,41 +1,36 @@
-import { useEffect ,useState } from "react";
+import { useEffect, useState } from "react";
 
-export const LoadingScreen = ( {onComplete} ) => {
- const [text, setText] = useState("")
- const fullText = "<Welcome!/>";
+export const LoadingScreen = ({ onComplete }) => {
+  const [text, setText] = useState("");
+  const fullText = "<Hello World/>";
 
- useEffect(() => {
-
+  useEffect(() => {
     let index = 0;
     const interval = setInterval(() => {
-      setText(fullText.substring(0, index))
+      setText(fullText.substring(0, index));
       index++;
 
-      if(index > fullText.length) {
-        clearInterval(interval)
+      if (index > fullText.length) {
+        clearInterval(interval);
 
         setTimeout(() => {
           onComplete();
         }, 2000);
       }
-
     }, 120);
 
-    return() => clearInterval(interval);
+    return () => clearInterval(interval);
+  }, [onComplete]);
 
- }, [onComplete]);
+  return (
+    <div className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center">
+      <div className="mb-4 ml-9 text-4xl font-mono font-bold">
+        {text} <span className="animate-blink ml-1"> | </span>
+      </div>
 
-
-  return <div className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center">
-
-    <div className="mb-4 ml-9 text-4xl font-mono font-bold">{text} <span className="animate-blink ml-1"> | </span></div>
-
-    <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
-      <div className="w-full h-full bg-purple-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar">
-        
+      <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
+        <div className="w-full h-full bg-purple-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar"></div>
       </div>
     </div>
-
-  </div>
-
-}
+  );
+};
